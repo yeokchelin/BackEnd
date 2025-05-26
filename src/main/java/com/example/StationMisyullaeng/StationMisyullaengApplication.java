81f1b0dd0afbb54f1947e74c0fd9f329d768c213
hun <kcnooes2@gmail.com>
2025-05-26 15:31:35 +0900

    20250526

---
+++ src/main/java/com/example/StationMisyullaeng/StationMisyullaengApplication.java
@@ -0,0 +1,13 @@
+package com.example.StationMisyullaeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationMisyullaengApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationMisyullaengApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StationMisyullaeng/StationMisyullaengApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StationMisyullaeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationMisyullaengApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
