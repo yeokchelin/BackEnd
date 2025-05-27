a12a533771d3b1fc3a7d046e943e6d7592206f6a
ikh0120 <lkh50635164@gmail.com>
2025-05-27 09:51:28 +0900

    변경사항 add

---
+++ src/main/java/com/example/StationMisyullaeng/entity/Store.java
@@ -0,0 +1,41 @@
+package com.example.StationMisyullaeng;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "store")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Store {
+
+    //변경사항
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "store_id")
+    private Integer storeId;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "category", length = 50)
+    private String category;
+
+    @Column(name = "address", nullable = false, length = 255)
+    private String address;
+
+    @Column(name = "phone", length = 20)
+    private String phone;
+
+    @Column(name = "open", length = 10)
+    private String open;
+
+    @Column(name = "close", length = 10)
+    private String close;
+
+    @Column(name = "rating")
+    private Float rating;
+}
