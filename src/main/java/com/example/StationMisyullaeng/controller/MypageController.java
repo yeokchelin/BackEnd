2aec24a409ab0b17f98cd7b4d10a1f39bd0006ec
ikh0120 <lkh50635164@gmail.com>
2025-06-02 16:29:59 +0900

    mypage에 favorite 병합

--- src/main/java/com/example/StationMisyullaeng/controller/MypageController.java
+++ src/main/java/com/example/StationMisyullaeng/controller/MypageController.java
@@ -11,34 +11,34 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/favorites")
+@RequestMapping("/mypage")
 @RequiredArgsConstructor
-public class FavoriteController {
+public class MypageController {
 
     private final FavoriteService favoriteService;
 
     //즐겨찾기 추가
-    @PostMapping
+    @PostMapping("/favorite")
     public ResponseEntity<FavoriteResponseDto> createFavorite(@RequestBody FavoriteRequestDto requestDto) {
         FavoriteResponseDto response = favoriteService.save(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
     //즐겨찾기 목록 모두보기
-    @GetMapping
+    @GetMapping("/favorite")
     public ResponseEntity<List<FavoriteResponseDto>> getAllFavorites() {
         return ResponseEntity.ok(favoriteService.findAll());
     }
 
     //각 유저 별 즐겨찾기 한 목록 조회
-    @GetMapping("/user/{userId}")
+    @GetMapping("/favorite/user/{userId}")
     public ResponseEntity<List<FavoriteResponseDto>> getFavoritesByUser(@PathVariable Long userId) {
         List<FavoriteResponseDto> favorites = favoriteService.getFavoritesByUser(userId);
         return ResponseEntity.ok(favorites);
     }
 
     //가게 별 즐겨찾기 조회
-    @GetMapping("/store/{storeId}")
+    @GetMapping("/favorite/store/{storeId}")
     public ResponseEntity<List<FavoriteResponseDto>> getFavoritesByStore(@PathVariable Long storeId) {
         List<FavoriteResponseDto> favorites = favoriteService.getFavoritesByStore(storeId);
         return ResponseEntity.ok(favorites);
@@ -47,10 +47,10 @@ public class FavoriteController {
 
 
     //즐겨찾기 삭제 //favorite 테이블의 기본키를 넣어야 삭제 완료
-    @DeleteMapping("/{favoriteId}")
+    @DeleteMapping("/favorite/{favoriteId}")
     public ResponseEntity<Void> deleteFavorite(@PathVariable Long favoriteId) {
         favoriteService.deleteFavorite(favoriteId);
         return ResponseEntity.noContent().build(); // 204 No Content
     }
-    
+
 }
